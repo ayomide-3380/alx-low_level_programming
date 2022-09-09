2959d44879b26373d8d5b87118c7e5cd44e972bf
ayomide-3380 <alaoayomide700@gmail.com>
2022-09-09 16:29:42 +0100

    ad task 6-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 0 to 9 with , ans ' '
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+  for (i = '0'; i <= '9'; i++)
+  {
+    putchar(i);
+
+      if (i != '9')
+      {
+        putchar(',');
+        putchar(' ');
+      }
+  }
+  putchar('\n');
+
+	return (0);
+}
