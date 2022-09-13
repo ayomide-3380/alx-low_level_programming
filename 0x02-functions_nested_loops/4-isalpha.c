cbe10832bdad67a1e5b6a799994c8aaf845a62f3
ayomide-3380 <alaoayomide700@gmail.com>
2022-09-13 14:49:35 +0100

    add files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character 
+ * @c: chracter to be checked
+ * @return 1 if successful , 0 if not
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a'; c <= 'z')) && ((C >= 'A'; C <= 'Z'));
+}
