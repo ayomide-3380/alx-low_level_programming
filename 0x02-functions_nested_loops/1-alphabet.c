e777ade8028921bd9b269b97cd37251c3e504fb6
ayomide-3380 <alaoayomide700@gmail.com>
2022-09-13 12:55:55 +0100

    add files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@
 
 void print_alphabet(void)
 {
-     char ch = 'a';
+	char ch = 'a';
 
-   while(ch <= 'z')  
-   {
-      putchar(ch);
-      ch++;
-   }
-    putchar('\n');
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
 }
