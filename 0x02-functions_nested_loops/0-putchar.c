be06632ed9fb964d75f6970556fb468355ddd3b7
ayomide-3380 <alaoayomide700@gmail.com>
2022-09-13 12:42:17 +0100

    add files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, and followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
+
+  return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * void print_alphabet(void) - print alphabet lowercase
+ * Return: Always (success)
+ */
+
+void print_alphabet(void)
+{
+     char ch = 'a';
+
+   while(ch <= 'z')  
+   {
+      putchar(ch);
+      ch++;
+   }
+    putchar('\n');
+}
