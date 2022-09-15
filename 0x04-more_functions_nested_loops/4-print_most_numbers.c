23f58f74dc0a39af76db7e8b7b8739c898e53c64
ayomide-3380 <alaoayomide700@gmail.com>
2022-09-15 15:02:01 +0100

    update file 4 & 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,38 @@
  * Return: none
  */
 void print_most_numbers(void)
-{
-	int c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (c != '2' && c != '4')
-		{
-			_putchar(c);
-		}
+{
+int i;
+int first_number;
+int second_number;
+int result;
+
+i = 0;
+result = 0;
+while (i < 10)
+{
+while (result <= 14)
+{
+if (result < 10)
+{
+second_number = result;
 }
 
-	_putchar('\n');
+else
+{
+first_number = result / 10;
+second_number = result % 10;
+_putchar (first_number + '0');
+}
+
+_putchar (second_number + '0');
+
+result++;
+}
+i++;
+result = 0;
+_putchar ('\n');
+}
+
 }
