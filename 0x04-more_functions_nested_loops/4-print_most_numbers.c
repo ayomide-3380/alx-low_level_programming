d8aebadf507539f764f7fd62e24b68e1fcb55392
ayomide-3380 <alaoayomide700@gmail.com>
2022-09-15 14:11:58 +0100

    add file 4 & 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print - numbers from 0 to 9 except 2 and 4
+ * Return: Alway (succes)
+ */	
+void print_most_numbers(void)
+{
+		int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+}
+
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print - number 0 to 14 x10
+ * Return: Always (success)
+ */
+void more_numbers(void)
+{
+	int number;
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+		for (number = '0'; number <= 14; number++)
+	
+		{
+			_putchar(number);
+		}
+
+		count++;
+		_putchar('\n');
+}
+
+}
