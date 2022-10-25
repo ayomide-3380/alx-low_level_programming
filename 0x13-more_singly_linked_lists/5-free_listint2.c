c2a5eb8fb97e7a02926276423518bc828eaf730b
ayomide-3380 <alaoayomide700@gmail.com>
2022-10-25 13:20:00 +0100

    update 3 tasks

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
-		while (*head)
-		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
-		}
-		*head = NULL;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
 }
