f09587d945c995941380cdbdc50e12e1c4dae3e2
ayomide-3380 <alaoayomide700@gmail.com>
2022-11-01 09:41:48 +0100

    update task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,14 +4,14 @@
 * append_text_to_file - a function that appends
 * @filename: the file name
 * @text_content: NULL terminated to string
-
+*
 * Return: 1 on success, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, t, s = 0;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
